5a40a0706a210d162aeab47915653ebd2a7e81e9
AdrianBartos19 <adrianbartos2004@gmail.com>
2026-02-05 20:47:46 +0100

    v1 hotova

--- app.js
+++ app.js
@@ -1,12 +1,92 @@
 console.log('app.js načítaný');
+
+function randomOneToThree() {
+    return Math.floor(Math.random()*3)+1;
+}
+
+
+function renderGrid(selectedArr) {
+    console.log("renderGrid: budem renderovať", selectedArr);
+}
+
+function renderOnePerSelected(selected) {
+    var grid = document.getElementById("grid");
+    // 1) vyčisti grid
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    // 2) prejdi všetky vybrané kategórie
+    var i;
+    for (i = 0; i < selected.length; i++) {
+        var category = selected[i];
+
+        // 2a) vygeneruj náhodné číslo 1..3
+        var n = randomOneToThree();
+
+        // 2b) poskladaj cestu k obrázku: images/<kategoria>/<n>.png
+        var src = "images/" + category + "/" + String(n) + ".png";
+
+        // 2c) vytvor kartu
+        var card = document.createElement("article");
+        card.className = "card";
+
+
+
+        // obrázok
+        var img = document.createElement("img");
+        img.className = "card__img";
+        img.alt = category + " – obrázok";
+        img.loading = "lazy";
+        img.src = src;
+
+        // poskladaj DOM štruktúru
+        card.appendChild(img);
+
+        // vlož do gridu
+        grid.appendChild(card);
+    }
+}
+
+console.log(randomOneToThree())
+
+
+const IMAGE_DATA = {
+    tricka:  ["1.png", "2.png", "3.png"],
+    nohavice:["1.png", "2.png", "3.png"],
+    topanky: ["1.png", "2.png", "3.png"],
+};
+
+const LABELS = {
+    tricka: "Tričká",
+    nohavice: "Nohavice",
+    topanky: "Topánky",
+}
+
+
+
+
 const form = document.getElementById("category-form")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    //FormData -> vstavany typ ktory ziska vsetky data formu
     const data = new FormData(form)
 
-    const selected = data.getAll("categories")
+    //getAll ziska vsetky zaskrtnute hodnoty -> ak neni zaskrtnute tak jeho value neni poslane
+    const selectedArr = data.getAll("categories");
 
-    console.log("Vybrané kategórie:", selected);
+    console.log("Vybrané kategórie:", selectedArr);
+
+
+    renderGrid(selectedArr);
+    renderOnePerSelected(selectedArr);
 });
+
+
+
+
+
+
